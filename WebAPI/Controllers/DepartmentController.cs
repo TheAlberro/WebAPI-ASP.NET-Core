2ea490461d7e9767134e95017fc7b7764147d705
Albert <albert.rachwal@pollub.edu.pl>
2022-07-20 12:50:53 +0200

    Dodaj pliki projektów.

---
+++ WebAPI/Controllers/DepartmentController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentController : ControllerBase
+    {
+
+        private readonly IConfiguration _configuration;
+        //private object myCommand;
+
+        public DepartmentController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        [HttpGet]
+        public JsonResult Get()
+        {
+            //Use stored procedures or EntityFramework Update this later
+            string query = @"
+                select DepartmentId, DepartmentName from dbo.Department";
+            DataTable table = new DataTable();
+            string sqlDatasource = _configuration.GetConnectionString("EmployeeAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDatasource))
+            {
+                
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+        return new JsonResult(table);
+        }
+        [HttpPost]
+        public JsonResult Post(Department dep)
+        {
+            //Use stored procedures or EntityFramework Update this later
+            string query = @"
+                insert into  dbo.Department values
+                    ('" + dep.DepartmentName + @"')
+                    ";
+            DataTable table = new DataTable();
+            string sqlDatasource = _configuration.GetConnectionString("EmployeeAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDatasource))
+            {
+
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult("Added Successful l");
+        }
+    }
+    
+}
